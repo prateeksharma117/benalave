061d7b0b80c9e7689e7863112c637d2c2163708a
Prateek <76089649+prateeksharma117@users.noreply.github.com>
2023-11-03 12:51:15 +0530

    server

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/',(req, res) => {
-    return res.status(200).send({message:"welcome to e-commerce "})
+    return res.status(200).send("i am started 😄")
 })
 
 app.use("/auth",authRouter)

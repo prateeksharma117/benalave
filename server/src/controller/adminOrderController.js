b2a7b0a5b3cba9c3651ad255fbc19ef9767c0d02
Prateek <76089649+prateeksharma117@users.noreply.github.com>
2023-11-03 11:07:04 +0530

    routes problem

--- server/src/controller/adminOrderController.js
+++ server/src/controller/adminOrderController.js
@@ -1,7 +1,7 @@
 import { cancelOrder, confirmedOrder, deleteOrder, deliverOrder, getAllOrder, shipOrder } from "../services/OrderService.js"
 
 
-export const getAllOrders =async (res,req) =>{
+export const getAllOrders =async (req, res) =>{
     try {
         const orders=await getAllOrder()
         return res.status(200).send(orders)
